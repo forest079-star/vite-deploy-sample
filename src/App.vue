4d15e6d5199ae02c01f0749785dc4c8f7a0444c4
forest079 <bluegx08@gmail.com>
2023-03-08 18:02:27 +0800

    將 components 引入並修正 pinia 引入和 連結

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,38 @@
 <script>
+import CartComponent from './components/CartComponent.vue';
 import NavbarComponent from './components/NavbarComponent.vue';
+import ProductsComponent from './components/ProductsComponent.vue';
+
+
 
 
 export default {
   components: {
     NavbarComponent,
+    ProductsComponent,
+    CartComponent
   },
 };
 </script>
 
 <template>
-  <NavbarComponent></NavbarComponent>
+  <div class="container">
+    <NavbarComponent></NavbarComponent>
+    <ProductsComponent></ProductsComponent>
+    <CartComponent></CartComponent>
+  </div>
 </template>
 
 <style lang="scss">
 @import "./assets/all.scss";
+.card-img-top {
+  object-fit: cover;
+  height: 200px;
+}
+
+.table-image {
+  width: 75px;
+  height: 75px;
+  object-fit: cover;
+}
 </style>
--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -16,16 +16,6 @@ import { mapState } from "pinia";
 import cartStore from "../stores/cartStore.js";
 
 export default {
-  template: `<nav class="navbar bg-light">
-    <div class="container-fluid">
-      <span class="navbar-brand" href="#">香香餅乾店</span>
-      <button type="button" class="btn nav-link">
-        購物車
-        <span class="badge rounded-pill bg-danger text-white">{{cart.length}}</span>
-      </button>
-    </div>
-  </nav>
-  `,
   computed: {
     ...mapState(cartStore, ['cart']),
   },
